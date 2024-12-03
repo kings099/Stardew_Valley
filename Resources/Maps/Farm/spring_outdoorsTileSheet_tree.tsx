168f7507e6fb10e583875fcece442be8cfb15d3a
momokos <3231288262@qq.com>
2024-12-03 16:21:35 +0800

    [Chore]Add a map
    add a orginal map

---
+++ Resources/Maps/Farm/spring_outdoorsTileSheet_tree.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="spring_outdoorsTileSheet_tree" tilewidth="16" tileheight="16" tilecount="1120" columns="16">
+ <image source="spring_outdoorsTileSheet2.png" width="256" height="1120"/>
+</tileset>
