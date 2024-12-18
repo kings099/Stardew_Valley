e638f25a259a8de80e1955257f279c9e3f0fc326
99 <147248330+kings099@users.noreply.github.com>
2024-12-19 02:02:09 +0800

    添加小镇地图

---
+++ Resources/Maps/Town/spring_Shadows.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="spring_Shadows" tilewidth="16" tileheight="16" tilecount="475" columns="19">
+ <image source="spring_Shadows.png" width="304" height="400"/>
+</tileset>
