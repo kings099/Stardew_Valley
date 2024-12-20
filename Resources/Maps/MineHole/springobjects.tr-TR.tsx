bca8dfc8c512e6dd3e145ba900a37049f2384d95
momokos <3231288262@qq.com>
2024-12-20 21:38:07 +0800

    [Bugfix]新增了矿洞地图

---
+++ Resources/Maps/MineHole/springobjects.tr-TR.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="springobjects.tr-TR" tilewidth="16" tileheight="16" tilecount="936" columns="24">
+ <image source="springobjects.tr-TR.png" width="384" height="624"/>
+</tileset>
