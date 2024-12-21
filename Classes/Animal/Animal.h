5a1e258e13cc28d8f0bd6edc107a251ad9222cd9
momokos <3231288262@qq.com>
2024-12-21 20:46:02 +0800

    [Bugfix]改正了一些代码不规范的问题

--- Classes/Animal/Animal.h
+++ Classes/Animal/Animal.h
@@ -1,8 +1,8 @@
 /****************************************************************
  * Project Name:  Stardew_Valley
  * File Name:     Animal.h
- * File Function: С�����࣬ʵ��С����ĸ��ֻ�������
- * Author:        ������
+ * File Function: 小动物类，实现小动物的各种基本操作
+ * Author:        胡宝怡
  * Update Date:   2024/12/17
  * License:       MIT License
  ****************************************************************/
@@ -20,64 +20,69 @@ USING_NS_CC;
 class Animal : public Node {
     
 protected:
-    Sprite* angryIcon; // ŭ��ͼ��
-    std::string type;   // �������� (�磺chicken��cow��sheep)
-    float affection;    // ����ĺøж�
-    bool isFed;         // �Ƿ�ι��
-    float lastFedTime;  // ���ι��ʱ��
-    float affectionDecayRate; // �øж�˥������
-    float maxAffection;    // ���øж�
+    Sprite* _angryIcon; // 动物发怒的图标
+    std::string _type;   // 动物类型
+    float _affection;    // 动物的好感度
+    bool _isFed;         // 是否被喂食
+    float _lastFedTime;  // 上次喂食的时间
+    float _affectionDecayRate; // 好感度衰减速度
+    float _maxAffection;    // 最大好感度
 
-    Sprite* sprite;   // ����ľ���
-    Action* currentAnimation; // ��ǰ���ŵĶ���
-    
-    Vec2 position;     // ����ĵ�ǰ����
-    Vec2 origin;       // ����С����ʱָ����ԭ�� (�����ε����½�)
-    float areaSize;    // ����������Ĵ�С (20x20)
+    Sprite* _sprite;   // 动物的精灵
+    Action* _currentAnimation; // 当前正在播放的动画
 
-    // �������Դӳ��
-    static std::unordered_map<std::string, std::vector<std::string>> resourceMap;
-    static std::unordered_map<std::string, std::unordered_map<std::string, std::vector<std::string>>> animationMap;
+    Vec2 _position;     // 动物当前位置
+    Vec2 _origin;       // 动物初始化时的原点 (地图的左下角坐标)
+    float _areaSize;    // 动物活动范围的大小 (例如20x20范围)
+
+    static std::unordered_map<std::string, std::vector<std::string>> _resourceMap;
+    // 静态资源映射表，用于存储动物类型与其资源路径的对应关系
+
+    static std::unordered_map<std::string, std::unordered_map<std::string, std::vector<std::string>>> _animationMap;
+    // 静态动画映射表，用于存储动物类型、动作方向和对应动画资源的映射
 
 public:
-    Vec2 direction;
-    Animal();
-    ~Animal();
+    Vec2 _direction; // 动物当前的移动方向
 
-  
+    Animal(); // 构造函数
+    ~Animal(); // 析构函数
 
-    // ��ʼ����Դ
+    // 初始化资源映射表
     static void initializeResourceMap();
+
+    // 初始化动画映射表
     static void initializeAnimationMap();
 
-    // ��������ʵ��
+    // 创建动物实例
     static Animal* create(const std::string& type, const Vec2& startPosition);
-    //��ʼ��������Ϣ
+
+    // 初始化动物信息
     bool init(const std::string& type, const Vec2& startPosition);
 
-    // ι������
+    // 喂食动物
     void feed();
 
-    // ���ºøж�
+    // 更新好感度
     void updateAffection(float deltaTime);
 
-    // �ж϶����Ƿ񷢿�
+    // 判断动物是否发怒
     bool isAngry() const;
 
-    // ���﷢��ʱ�Ĵ���
+    // 处理动物发怒时的逻辑
     void handleAngry();
 
-    // ��ȡ���ﵱǰ�øж�
+    // 获取动物当前的好感度
     float getAffection() const;
 
-    // ��ȡ��������
+    // 获取动物类型
     const std::string& getType() const;
 
-    // ���ö��������ƶ�
+    // 设置动物的随机移动
     void setRandomMovement(float deltaTime);
 
-    // ���ŷ��򶯻�
+    // 播放方向动画
     void playDirectionAnimation(const std::string& direction, int repeatCount);
+
 };
 
 #endif // ANIMAL_H
